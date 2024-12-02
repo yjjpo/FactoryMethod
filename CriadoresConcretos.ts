cbc5616a509fd040c90926d8ef186a1fde2c3b44
yjjpo <agnesyasmini11@gmail.com>
2024-12-02 11:07:40 -0300

    Create CriadoresConcretos.ts

---
+++ CriadoresConcretos.ts
@@ -0,0 +1,23 @@
+class EditorPDF extends EditorArquivo {
+    criarArquivo(): Arquivo {
+        return new ArquivoPDF();
+    }
+}
+
+class EditorDOCX extends EditorArquivo {
+    criarArquivo(): Arquivo {
+        return new ArquivoDOCX();
+    }
+}
+
+class EditorXLSX extends EditorArquivo {
+    criarArquivo(): Arquivo {
+        return new ArquivoXLSX();
+    }
+}
+
+class EditorTXT extends EditorArquivo {
+    criarArquivo(): Arquivo {
+        return new ArquivoTXT();
+    }
+}
