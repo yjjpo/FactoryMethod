64d073f95c25ebd513370866b8eeca7e1aec525a
yjjpo <agnesyasmini11@gmail.com>
2024-12-02 11:06:59 -0300

    Create Produtos.ts
    Implementações do contrato

---
+++ Produtos.ts
@@ -0,0 +1,39 @@
+class ArquivoPDF implements Arquivo {
+    abrir(): void {
+        console.log("Abrindo arquivo PDF...");
+    }
+
+    salvar(): void {
+        console.log("Salvando arquivo PDF...");
+    }
+}
+
+class ArquivoDOCX implements Arquivo {
+    abrir(): void {
+        console.log("Abrindo arquivo DOCX...");
+    }
+
+    salvar(): void {
+        console.log("Salvando arquivo DOCX...");
+    }
+}
+
+class ArquivoXLSX implements Arquivo {
+    abrir(): void {
+        console.log("Abrindo arquivo XLSX...");
+    }
+
+    salvar(): void {
+        console.log("Salvando arquivo XLSX...");
+    }
+}
+
+class ArquivoTXT implements Arquivo {
+    abrir(): void {
+        console.log("Abrindo arquivo TXT...");
+    }
+
+    salvar(): void {
+        console.log("Salvando arquivo TXT...");
+    }
+}
